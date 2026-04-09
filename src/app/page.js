9afe67641a6d0ce62c57bd1e4430f70956d43e65
sara91-dev <sarabog91@gmail.com>
2026-04-09 10:24:54 +0200

    Parágrafo y botón de la hero section puestos

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,14 @@ export default function Home() {
       <main className="mt-10 lg:mt-20 xl:mt-30">
         {/* HERO SECTION */}
         <div className="flex flex-col items-center">
-            <h1 className="font-semibold tracking-wide text-2xl md:text-3xl lg:text-4xl">JEJEJEJEJ JEJEJ</h1>
-            <h2 className="mt-2 font-normal text-base md:text-lg lg:text-xl">jejejee</h2>
+            <h1 className="font-semibold tracking-wide text-xl md:text-2xl xl:text-3xl">JEJEJEJEJ JEJEJ</h1>
+            <h2 className="mt-1.5 font-normal text-base md:text-lg xl:text-xl">jejejee</h2>
+            <p className="mt-8 text-sm xl:text-base">saddassdsads gjffg jfjgf sbsb irer</p>
+            <p className="mt-1.5 text-sm xl:text-base">saddassdsads gjffg jfjgf sbsb irer fedsf</p>
+            <p className="mt-1.5 text-sm xl:text-base">saddassdsads gjffg jfjgf sbsb irer dfsf fdssd</p>
+            <button className="mt-4 bg-white px-6 py-1 text-sm rounded-lg hover:animate-pulse cursor-pointer text-gray-700">
+            aasfjf
+            </button>
         </div>
       </main>
   );
